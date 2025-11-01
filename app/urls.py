250cc1ae3f35a282bbfcefcf350c35c17c4fd4c2
Sameer Kongath <zoomixinfo@gmail.com>
2025-11-01 22:59:30 +0530

    show, edit, delete added

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('add/', views.add, name='add'),
+    path('show/<int:pk>/', views.show, name='show'),
+    path('edit/<int:pk>/', views.edit, name='edit'),
+    path('delete/<int:pk>/', views.delete, name='delete'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from . models import Product
 from . forms import ProductForm
 
@@ -14,3 +14,22 @@ def add(request):
     else:
         form = ProductForm()
     return render(request, 'add.html', {'form': form})
+def show(request, pk):
+    product = Product.objects.get(pk=pk)
+    return render(request, 'show.html', {'product': product})
+def edit(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('show', pk=product.pk)
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'edit.html', {'form': form, 'product': product})
+def delete(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        product.delete()
+        return redirect('index')
+    return render(request, 'delete.html', {'product': product})
